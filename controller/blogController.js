794dbc67ecf244d31a55bd780db076723d36674b
rathore90 <rathore90@hotmail.com>
2022-10-27 20:10:55 -0700

    push

--- controller/blogController.js
+++ controller/blogController.js
@@ -7,7 +7,65 @@ const blog_index = (req, res)=> {
    });
 }
 
+const secured_endpoint = async(req, res) => {
+  let data = {}
+  const { token_type, access_token } = req.oidc.accessToken;
+
+  try{
+      // calling the server to get the data, make sure you get the data before moving forward(async, await)
+      const apiResponse = await axios.get('http://localhost:5000/private', {
+          headers: {
+              authorization: `${token_type} ${access_token}`
+          }
+      });
+      data = apiResponse.data;
+      access_token.scope.push('read:messages');
+  }catch(e) {
+      console.log(e);
+  }
+  console.log(access_token);
+  
+  // when there is not error, you will be redirected to the secured page with the data you get fromt the api
+  res.render('secured', {
+      title: "Secured Page",
+      isAuthenticated: req.oidc.isAuthenticated(),
+      data
+  })
+}; 
+
+const create_record = async(req, res) => {
+  let data = {}
+  const { token_type, access_token } = req.oidc.accessToken;
+
+  try{
+      // calling the server to get the data, make sure you get the data before moving forward(async, await)
+      const apiResponse = await axios.get('http://localhost:5000/role', {
+          headers: {
+              authorization: `${token_type} ${access_token}`
+          }
+      });
+      data = apiResponse.data;
+
+      // when there is not error, you will be redirected to the secured page with the data you get fromt the api
+      res.render('create', {
+          title: 'Admin User', 
+          isAuthenticated: req.oidc.isAuthenticated(),
+          user: req.oidc.user,
+          data: data
+      })
+  }catch(e) {
+      console.log(e);
+      res.render('notAccess', {
+          title: 'Not Access Page',
+          isAuthenticated: req.oidc.isAuthenticated()
+        });
+  }
+  
+}
+
   module.exports = {
-    blog_index
+    blog_index,
+    secured_endpoint,
+    create_record
   }
   
--- routes/index.js
+++ routes/index.js
@@ -2,66 +2,11 @@ var express = require("express");
 var router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 const axios = require('axios');
-const blogController = require('./../controller/blogController')
+const blogController = require('./../controller/blogController');
 
+// Application routes
 router.get('/', blogController.blog_index);
-
-// trigger the endoint, and call the middleware, if the user is logged in or not
-router.get('/secured', requiresAuth(), async(req, res) => {
-    let data = {}
-    const { token_type, access_token } = req.oidc.accessToken;
-
-    try{
-        // calling the server to get the data, make sure you get the data before moving forward(async, await)
-        const apiResponse = await axios.get('http://localhost:5000/private', {
-            headers: {
-                authorization: `${token_type} ${access_token}`
-            }
-        });
-        data = apiResponse.data;
-        access_token.scope.push('read:messages');
-    }catch(e) {
-        console.log(e);
-    }
-    console.log(access_token);
-    
-    // when there is not error, you will be redirected to the secured page with the data you get fromt the api
-    res.render('secured', {
-        title: "Secured Page",
-        isAuthenticated: req.oidc.isAuthenticated(),
-        data
-    })
-}) 
-
-// Create
-router.get('/create', requiresAuth(), async(req, res) => {
-    let data = {}
-    const { token_type, access_token } = req.oidc.accessToken;
-
-    try{
-        // calling the server to get the data, make sure you get the data before moving forward(async, await)
-        const apiResponse = await axios.get('http://localhost:5000/role', {
-            headers: {
-                authorization: `${token_type} ${access_token}`
-            }
-        });
-        data = apiResponse.data;
-
-        // when there is not error, you will be redirected to the secured page with the data you get fromt the api
-        res.render('create', {
-            title: 'Admin User', 
-            isAuthenticated: req.oidc.isAuthenticated(),
-            user: req.oidc.user,
-            data: data
-        })
-    }catch(e) {
-        console.log(e);
-        res.render('notAccess', {
-            title: 'Not Access Page',
-            isAuthenticated: req.oidc.isAuthenticated()
-          });
-    }
-    
-}) 
+router.get('/secured', requiresAuth(), blogController.secured_endpoint);
+router.get('/create', requiresAuth(), blogController.create_record)
 
 module.exports = router;
